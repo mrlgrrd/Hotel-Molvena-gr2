d79918a2f19c3782621245e1fa7a76aa9026d727
mrlgrrd <35731373+mrlgrrd@users.noreply.github.com>
2018-02-19 16:00:43 +0100

    Initial commit

---
+++ src/main/java/com/capgemini/Hotel/Molvena/gr2/HotelMolvenaGr2Application.java
@@ -0,0 +1,12 @@
+package com.capgemini.Hotel.Molvena.gr2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMolvenaGr2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelMolvenaGr2Application.class, args);
+	}
+}
