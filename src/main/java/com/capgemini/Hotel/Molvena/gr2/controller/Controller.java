5b44e3fbd24f45c9f603db48b5411298d7bd148b
mrlgrrd <35731373+mrlgrrd@users.noreply.github.com>
2018-02-22 15:17:58 +0100

    update room repository

--- src/main/java/com/capgemini/Hotel/Molvena/gr2/controller/Controller.java
+++ src/main/java/com/capgemini/Hotel/Molvena/gr2/controller/Controller.java
@@ -2,14 +2,10 @@ package com.capgemini.Hotel.Molvena.gr2.controller;
 
 import com.capgemini.Hotel.Molvena.gr2.ERoomType;
 import com.capgemini.Hotel.Molvena.gr2.Room;
-
 import com.capgemini.Hotel.Molvena.gr2.person.Guest;
-
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.capgemini.Hotel.Molvena.gr2.repositories.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -17,45 +13,18 @@ import java.util.ArrayList;
 @RequestMapping("/api/controller/")
 public class Controller {
 
-    private ArrayList<Room> rooms = new ArrayList<Room>();
     private Room room;
     private ArrayList<Guest> guests = new ArrayList<Guest>();
-
     private Guest guest;
-
-    /**
-     * initialize the rooms
-     *
-     * @return room
-     */
-    @RequestMapping("initializerooms")
-    public ArrayList<Room> initializeRooms() {
-
-        Room room1 = new Room(ERoomType.STANDARD, 1);
-        Room room2 = new Room(ERoomType.STANDARD, 2);
-        Room room3 = new Room(ERoomType.DELUXE, 3);
-        Room room4 = new Room(ERoomType.DELUXE, 4);
-        Room room5 = new Room(ERoomType.HONEYMOON, 5);
-        rooms.add(room1);
-        rooms.add(room2);
-        rooms.add(room3);
-        rooms.add(room4);
-        rooms.add(room5);
-
-        return rooms;
-    }
+    @Autowired
+    private RoomRepository roomRepository;
 
     /**
      * add a rom to the arraylist of rooms
-     *
-     * @param ERoomType enum roomtype
-     * @param number    room number
-     * @return list of rooms
      */
-    @RequestMapping("addroom")
-    public ArrayList<Room> addRoom(Enum<ERoomType> ERoomType, int number) {
-        rooms.add(new Room(ERoomType, number));
-        return rooms;
+    @RequestMapping(value = "addroom", method = RequestMethod.POST)
+    public void addRoom(@RequestBody Room roomToSave) {
+        roomRepository.saveRoom(roomToSave);
     }
 
     /**
@@ -63,68 +32,21 @@ public class Controller {
      *
      * @param number    room number
      * @param ERoomType room type
-     * @return room
      */
-    @RequestMapping("changeroom")
-    public Room changeRoom(int number, Enum<ERoomType> ERoomType) {
-        if (room.getNumber() == number) {
-            this.room.setRoomType(ERoomType);
-        }
-        return room;
-    }
-
-    @RequestMapping("showrooms")
-    public ArrayList<Room> showRooms(){
-        return rooms;
+    @RequestMapping(value = "updateroom", method = RequestMethod.POST)
+    public void updateRoom(@RequestBody int number, @RequestBody Enum<ERoomType> ERoomType) {
+        roomRepository.updateRoom(number, ERoomType);
     }
 
 
+    /**
+     * this method gets the list of rooms from the repository
+     */
+    @RequestMapping(value = "showrooms", method = RequestMethod.GET)
+    public void showRooms() {
+        roomRepository.getRooms();
 
-    @GetMapping("/api/addGuest")
-    public void addGuest(@RequestParam Guest guestToSave){
-
-        //return "Hello there " + name;
     }
-
-
-    //Geknipt en geplakt uit Room
-//    public ArrayList<Room> getRooms() {
-//        return rooms;
-//    }
-//
-//    //Make a list of rooms
-//    ArrayList<Room> roomList = new ArrayList<Room>();
-//
-//    /**
-//     * Methods that belong to room
-//     */
-//
-//    /**
-//     * this method checks which rooms are available in a given period
-//     */
-//    public void checkIfAvailable() {
-//
-//    }
-//
-//    /**
-//     * this method lets you modify the properties of the room
-//     *
-//     * @param number    the room number
-//     * @param Eroomtype the room type
-//     */
-//    public void changeRoom(int number, Enum<ERoomType> Eroomtype) {
-//        //this.roomType = Eroomtype;
-//        //this.number = number;
-//
-//    }
-//
-//    /**
-//     * this method removes a room from the system
-//     */
-//    public void removeRoom() {
-//
-//    }
-
 
     /**
      * delete a room from the array list of rooms
@@ -133,9 +55,14 @@ public class Controller {
      */
     @RequestMapping("deleteroom")
     public void deleteRoom(int number) {
-        if (room.getNumber() == number) {
-            rooms.remove(room);
-        }
+        roomRepository.deleteRoom(number);
+    }
+
+
+    @GetMapping("/api/addGuest")
+    public void addGuest(@RequestParam Guest guestToSave) {
+
+        //return "Hello there " + name;
     }
 
     /**
@@ -143,14 +70,6 @@ public class Controller {
      *
      * @return properties
      */
-    public ArrayList<Room> getRooms() {
-        return rooms;
-    }
-
-    public void setRooms(ArrayList<Room> rooms) {
-        this.rooms = rooms;
-    }
-
     public Room getRoom() {
         return room;
     }
