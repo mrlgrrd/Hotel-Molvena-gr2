5469b90919b360e0efc79c8f35fece02a0f32e67
tjitjert <rich.hoving@gmail.com>
2018-02-28 16:39:14 +0100

    booking controller

---
+++ src/main/java/com/capgemini/Hotel/Molvena/gr2/controller/BookingController.java
@@ -0,0 +1,14 @@
+package com.capgemini.Hotel.Molvena.gr2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/bookingcontroller/")
+public class BookingController {
+
+
+
+
+
+}
