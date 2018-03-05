973ede483550c2d66135cb8ca336ab90c9c84957
mhstrokappe <strokappemaarten@gmail.com>
2018-03-05 10:26:41 +0100

    update search guest

--- src/main/java/com/capgemini/Hotel/Molvena/gr2/repositories/GuestRepository.java
+++ src/main/java/com/capgemini/Hotel/Molvena/gr2/repositories/GuestRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public interface GuestRepository extends CrudRepository<Guest,Long> {
-    ArrayList<Guest> findByFirstnameContainingIgnoreCaseOrPrepositionContainingIgnoreCaseOrLastnameContainingIgnoreCaseOrAddressContainingIgnoreCaseOrZipCodeContainingIgnoreCaseOrCityContainingIgnoreCaseOrCountryContainingIgnoreCaseOrPhoneContainingIgnoreCaseOrEmailContainingIgnoreCase(String searchFirstname, String searchPreposition, String searchLastname, String searchAddress, String searchZipCode, String searchCity, String searchCountry, String searchPhone, String searchEmail);
-    ArrayList<Guest> findByFirstnameContainingIgnoreCaseAndLastnameContainingIgnoreCase(String searchFirstname, String searchLastname);
-    ArrayList<Guest> findByFirstnameContainingIgnoreCaseAndPrepositionContainingIgnoreCaseAndLastnameContainingIgnoreCase(String searchFirstname,String searchPreposition, String searchLastname);
+    List<Guest> findByFirstnameContainingIgnoreCaseOrPrepositionContainingIgnoreCaseOrLastnameContainingIgnoreCaseOrAddressContainingIgnoreCaseOrZipCodeContainingIgnoreCaseOrCityContainingIgnoreCaseOrCountryContainingIgnoreCaseOrPhoneContainingIgnoreCaseOrEmailContainingIgnoreCase(String searchFirstname, String searchPreposition, String searchLastname, String searchAddress, String searchZipCode, String searchCity, String searchCountry, String searchPhone, String searchEmail);
+    //ArrayList<Guest> findByFirstnameContainingIgnoreCaseAndLastnameContainingIgnoreCase(String searchFirstname, String searchLastname);
+    //ArrayList<Guest> findByFirstnameContainingIgnoreCaseAndPrepositionContainingIgnoreCaseAndLastnameContainingIgnoreCase(String searchFirstname,String searchPreposition, String searchLastname);
 
 }
